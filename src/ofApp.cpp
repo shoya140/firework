4f170eb3a2026a0186b6c80d3c40681724378e1f
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2015-03-29 16:48:54 +0900

    init

---
+++ src/ofApp.cpp
@@ -0,0 +1,79 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofSetCircleResolution(32);
+    
+    ofBackground(0,0,0);
+    nBandsToGet = 512;
+    
+    mySound.loadSound("perfume.wav");
+    mySound.setLoop(true);
+    mySound.play();
+    ofSetLineWidth(4);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    ofSoundUpdate();
+    fft = ofSoundGetSpectrum(nBandsToGet);
+    angle ++;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofTranslate(float(ofGetWidth())/2, ofGetHeight()/2, -2000);
+    ofRotateZ(-angle/10);
+    float width = float(ofGetWidth())/float(nBandsToGet)/2.0f;
+    for(int i=0;i<nBandsToGet;i++){
+        ofSetColor(ofColor::fromHsb(i/2, 140, 255, 100+fft[0]*500));
+        ofRotateZ(angle/10);
+        ofLine(width*i*3,(ofGetHeight())/2,width*i*3+fft[i],(ofGetHeight()+fft[i]*-300)/2);
+    }
+}
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
